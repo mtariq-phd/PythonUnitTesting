30006db42cf84878bb7dc03cb581092cdc206076
Maira Tariq <mtariq-phd>
2025-11-19 15:12:20 +0000

    Changes to factorial fucntion (recursion depth limit in Python) and  unit testing function (added mutliple tests and a test for exceptions)

--- mymath/factorial.py
+++ mymath/factorial.py
@@ -5,7 +5,24 @@ def factorial(n):
     :param int n: The factorial to calculate
     :return: The resultant factorial
     """
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return  n * factorial(n-1)
+
+    # # Limit for this implementation is 999 (due to the recursion depth 
+    # - because of how Python manages function calls and its memory stack)
+    # if n == 0 or n == 1:
+    #     return 1
+    # else:
+    #     return  n * factorial(n-1)
+    
+    #  implementation works for larger numbers (due to No Stack Limitation 
+    # and Faster and Less Overhead than Recursion)
+    #  scope of the function: limit is the afctorial can only have less than 
+    # 4300 digits)
+
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+    
+    factorial = 1
+    for i in range(1, n+1):
+        factorial *= i
+    return factorial
+
--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,8 +1,22 @@
 import unittest
 from mymath.factorial import factorial
+"""
+RUN from main directory:
+    python -m unittest tests/test_factorial.py
+    python -m unittest -v tests/test_factorial.py
+"""
 
-
+#  Unit test class -> must make sure the right logic (or answer) is provided, as the 
+# test will fail incorrectly otherwise -> both the code & test code must be correct
 class TestFactorialFunctions(unittest.TestCase):
 
     def test_3(self):
         self.assertEqual(factorial(3), 6)
+
+    def test_5(self):
+        self.assertEqual(factorial(5), 120)
+
+#  test for exceptions
+    def test_negative(self):
+        with self.assertRaises(ValueError):
+            factorial(-1)
